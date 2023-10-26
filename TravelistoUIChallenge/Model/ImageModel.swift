a50f7d4f139d9b6f67cdb9c59ec15577374bafd2
Rachel Radford <rachelradford21@gmail.com>
2023-10-26 15:57:51 -0400

    Added more images for the horizontal and implemented a custom scroll indicator that matches the design file

---
+++ TravelistoUIChallenge/Model/ImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModel.swift
+//  TravelistoUIChallenge
+//
+//  Created by Rachel Radford on 10/26/23.
+//
+
+import Foundation
---
+++ TravelistoUIChallenge/View/TutorialScreen/SignInNavLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInNavLinkView.swift
+//  TravelistoUIChallenge
+//
+//  Created by Rachel Radford on 10/26/23.
+//
+
+import SwiftUI
+
+struct SignInNavLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInNavLinkView()
+}
